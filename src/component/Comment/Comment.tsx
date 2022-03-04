ade7651fdd6bf9b3d4c17dd15ba359275ce1e569
DABIN JEONG <41@b41.kr>
2022-03-05 05:15:33 +0900

    Add - 코멘트 컴포넌트 마크업

--- src/component/Comment/Comment.tsx
+++ src/component/Comment/Comment.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import * as S from "./Comment.styled";
 
-const Comment = () => {
+const Comment = (comments: []) => {
+  console.log(comments);
   return (
     <S.Wrapper>
       <S.BtnIcon>댓글 2개</S.BtnIcon>
@@ -12,7 +13,9 @@ const Comment = () => {
           <S.Date>2022.03.05 00:48:53 작성</S.Date>
         </S.WriteInfoWrapper>
       </S.WriterWrapper>
-      <S.Content>네, 괜찮아요.</S.Content>
+      <S.Content>
+        <pre>네, 괜찮아요.</pre>
+      </S.Content>
     </S.Wrapper>
   );
 };
