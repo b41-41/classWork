f6d337bf01e90edfbeff9c79fbc7ab7308d5b02e
DABIN JEONG <41@b41.kr>
2022-03-06 05:25:33 +0900

    Refector - 안 쓰는 코드 삭제

--- src/component/Router.js
+++ src/component/Router.js
@@ -9,40 +9,6 @@ const AppRouter = () => {
     //redux
     const userState = useSelector(state => state.userInfo);
     const isLogin = userState.isLogin;
-
-    // 모바일 메뉴 열기, 닫기 스크립트
-    const [navBar, setNavBar] = useState(false);
-    const mobileMenuOpen = () => {
-        if (navBar) {
-            document.querySelector('.navBar').style.display = 'none';
-            setNavBar(false);
-        } else {
-            document.querySelector('.navBar').style.display = 'block';
-            setNavBar(true);
-        }
-    };
-
-    // 브라우저 resize할 때, 메뉴 open, close
-    const handleMobileMenu = () => {
-        if (window.innerWidth <= 800) {
-            document.querySelector('.navBar').style.display = 'none';
-            setNavBar(false);
-        } else {
-            document.querySelector('.navBar').style.display = 'block';
-            setNavBar(true);
-        }
-    };
-
-    useEffect(() => {
-        if (isLogin) {
-            window.addEventListener('resize', handleMobileMenu);
-            document.querySelector('.ulMenu').addEventListener('click', handleMobileMenu);
-            return () => {
-                window.removeEventListener('resize', handleMobileMenu);
-            }
-        }
-    }, []);
-
     return (
         <BrowserRouter basename="/">
             <Switch>
