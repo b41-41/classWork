51b1c6e7681892f514ba6c008125b10725cbe8d2
DABIN JEONG <41@b41.kr>
2022-03-05 18:32:09 +0900

    Refector - AppRouter 안 쓰는 코드 삭제

--- src/component/Router.js
+++ src/component/Router.js
@@ -1,20 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter, Router, Switch, Route } from "react-router-dom";
-import Home from 'routes/Home';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from 'component/Login';
-import Homework from 'routes/Homework';
-import Study from 'routes/Study';
-import Question from 'routes/Question'
-import Notice from 'routes/Notice';
-import Navigation from 'component/Layout/Navigation';
-import Admin from 'component/Admin';
-import listPNG from '../img/list.png';
 import Layout from 'component/Layout';
 
 
 const AppRouter = ({ isLoggedIn }) => {
 
-    // 모바일 메뉴 열기, 닫기 스크립트
+    // 모바일 메뉴 열기, 닫기 스크립트
     const [navBar, setNavBar] = useState(false);
     const mobileMenuOpen = () => {
         if (navBar) {
@@ -50,34 +42,7 @@ const AppRouter = ({ isLoggedIn }) => {
     return (
         <BrowserRouter basename="/">
             <Switch>
-                {
-                    isLoggedIn
-                        ? <>
-                            {/* <button type="button" className="menuBTN" onClick={mobileMenuOpen}>
-                                <span className="icon">
-                                    <img width="20px" src={listPNG} alt="notification" />
-                                </span>
-                            </button>
-                            <div className="box">
-                                <nav>
-                                    <Navigation />
-                                </nav>
-                                <div className="main">
-                                    < Route exact path="/" component={Home} />
-                                    < Route path="/Homework" component={Homework} />
-                                    < Route path="/Study" component={Study} />
-                                    < Route path="/Question" component={Question} />
-                                    < Route path="/Notice" component={Notice} />
-                                    < Route path="/Admin" componenet={Admin} />
-                                </div>
-                            </div>
-                            <div className="copyright">
-                                <footer>&copy; Class Work {new Date().getFullYear()}</footer>
-                            </div> */}
-                            <Layout />
-                        </>
-                        : <Route exact="exact" path="/"><Login /></Route>
-                }
+                {isLoggedIn ? <Layout /> : <Route exact="exact" path="/"><Login /></Route>}
             </Switch>
         </BrowserRouter>
     )
