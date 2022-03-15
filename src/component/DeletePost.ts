c823f55fea75f6e46d04b803961ba4f857317ef5
DABIN JEONG <41@b41.kr>
2022-03-15 20:15:08 +0900

    Add - 글 삭제 컴포넌트 추가

---
+++ src/component/DeletePost.ts
@@ -0,0 +1,11 @@
+import { doc, deleteDoc } from "firebase/firestore";
+import { dbService } from 'fbase';
+
+const DeleteComment = async (menuId: string, postId: string): Promise<void> => {
+    const commentRef = doc(dbService, menuId, postId);
+    await deleteDoc(commentRef);
+    alert('삭제완료');
+    window.location.reload();
+};
+
+export default DeleteComment;
--- src/component/index.ts
+++ src/component/index.ts
@@ -3,3 +3,4 @@ export { default as Comment } from "./Comment/Comment";
 export { default as CommentWriteForm } from "./Comment/CommentWriteForm";
 export { default as LoginInfoForm } from "./LoginInfoForm/LoginInfoForm";
 export { default as DeleteComment } from "./DeleteComment";
+export { default as DeletePost } from "./DeletePost";
